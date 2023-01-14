d653c031d8780392fada30340969c4dd617cbf76
spooketti <jonliu168@gmail.com>
2023-01-14 14:24:26 -0800

    what am i doing wrong

---
+++ script.js
@@ -0,0 +1,11 @@
+let board = document.getElementById("board")
+for(let i=0;i<4;i++)
+{
+    for(let j=0;j<4;j++)
+    {
+        let box = document.createElement("div")
+        board.append("box")
+    }
+}
+
+const api_key = "AIzaSyDl0YaBbeeilHPz4n-39g34XQxsLLjDwsw"
